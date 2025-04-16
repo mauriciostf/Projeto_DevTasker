6c5c6907aad232f50091f7adb5e2be7ebb5654c0
Caua Rodrigues <rodrigzs.soare@gmail.com>
2025-04-16 20:27:59 -0300

    data source - caua

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,19 +1,17 @@
-// import "reflect-metadata";
-// import { DataSource } from "typeorm";
-// import dotenv from "dotenv";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 
-// const { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
-
-// export const AppDataSource = new DataSource({
-//   type: "mysql",
-//   host: DB_HOST,  
-//   port: Number(DB_PORT), 
-//   username: DB_USER,  
-//   password: DB_PASSWORD, 
-//   database: DB_NAME,  
-//   entities: ["src/migrations/*.ts"],  
-//   synchronize: true,  
-//   logging: false,  
-// });
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_NAME,  
+  port: Number(process.env.DB_PORT), 
+  username: process.env.DB_USER,  
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["src/models/*.ts"],
+  synchronize: true,
+  logging: false, 
+});
