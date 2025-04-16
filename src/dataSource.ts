737d1aec94671d3d5d94a662dd94e0e75cc081bf
04433213020 <04433213020@senacrs.edu.br>
2025-04-16 19:27:25 -0300

    arquivo env e data source

---
+++ src/dataSource.ts
@@ -0,0 +1,19 @@
+// import "reflect-metadata";
+// import { DataSource } from "typeorm";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASSWORD } = process.env;
+
+// export const AppDataSource = new DataSource({
+//   type: "mysql",
+//   host: DB_HOST,  
+//   port: Number(DB_PORT), 
+//   username: DB_USER,  
+//   password: DB_PASSWORD, 
+//   database: DB_NAME,  
+//   entities: ["src/migrations/*.ts"],  
+//   synchronize: true,  
+//   logging: false,  
+// });
